f2688b38d1a1a311d8b111c3fa867e233ac63164
jeonhyeonu <tkawnd111@gmail.com>
2020-03-19 18:34:02 +0900

    Make Food-realted Class;    Domain;       Food    eating;       Domino, KFC,Zokbal    repository;       FoodRepository    web;       FoodListResponseDto    Sevice;       FoodService

---
+++ src/main/java/com/example/demo/domain/Food.java
@@ -0,0 +1,41 @@
+package com.example.demo.domain;
+
+import com.fasterxml.jackson.databind.ser.Serializers;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE )
+@DiscriminatorColumn(name = "Ftype")
+@Getter
+@Setter
+@NoArgsConstructor
+@Embeddable
+public class Food extends BaseTimeEntity{ //builder로 값을 넘겨줘야함.
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동으로 인덱스 ++ 만드는것
+    @Column(name = "food_id")
+    private Long id;
+
+    private String name;
+    private int price;
+
+    @ManyToMany(mappedBy = "foods")
+    private List<Category> categories = new ArrayList();
+
+    @ManyToOne(fetch = FetchType.LAZY) //member 과 order을 n:1로 매핑시킨다
+    @JoinColumn(name = "member_id") //외래키생성. many에서만 생성된다.
+    private Member member;
+
+    @Builder // 값변경.
+    public Food(Long id,String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+}
---
+++ src/main/java/com/example/demo/repository/FoodRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.Food;
+import com.example.demo.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface FoodRepository extends JpaRepository<Food, Long> {
+
+    @Query("SELECT m FROM Food m")
+    Food findOne(Long id);
+}
