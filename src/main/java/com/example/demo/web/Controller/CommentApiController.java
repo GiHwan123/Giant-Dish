f55d5bce266e503a8e11044a5d0546f550bfa8b5
jeonhyeonu <tkawnd111@gmail.com>
2020-03-19 20:29:35 +0900

    Guest Comment Sucess

---
+++ src/main/java/com/example/demo/web/Controller/CommentApiController.java
@@ -0,0 +1,21 @@
+package com.example.demo.web.Controller;
+
+import com.example.demo.service.CommentService;
+import com.example.demo.web.Request.CommentSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class CommentApiController {
+
+    private final CommentService commentService;
+
+        //아작스통신으로 값을 넘겨줘서 저장.
+        @PostMapping("/Guest/comment") //댓글을 저장함
+        public Long save(@RequestBody CommentSaveRequestDto requestDto){
+            return commentService.save(requestDto);
+        }
+}
